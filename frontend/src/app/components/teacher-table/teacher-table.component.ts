5c3e79c99c4b32c962b90a21c0c24f1cad4c0a73
Methuka-Perera <methukayonal@gmail.com>
2023-08-07 17:26:15 +0530

    frontend-test

--- frontend/src/app/components/teacher-table/teacher-table.component.ts
+++ frontend/src/app/components/teacher-table/teacher-table.component.ts
@@ -66,7 +66,7 @@ export class TeacherTableComponent implements OnInit {
       this.getTeacherData();
     } else {
       let b = this.teacherData.filter((teacher) => {
-        if (teacher[0].name.toLowerCase().indexOf(value) > -1) {
+        if (teacher[0].name.toLowerCase().includes(value.toLowerCase())) {
           foundItems.push(teacher)
         }
       });
